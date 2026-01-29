df5296a9c25252d027d8db5f65409e899bd71bbb
aantoine01 <augustin.antoine8@gmail.com>
2026-01-29 17:37:01 +0100

    Modification de la classe Customer en Classe SQL

---
+++ src/Modele/Class_SQL/Customer_SQL.py
@@ -0,0 +1,23 @@
+from Modele.SQLManager import Base, SessionLocal
+from sqlalchemy import ForeignKey, Column, Integer, String, Date
+from sqlalchemy.orm import declarative_base, sessionmaker
+from datetime import date
+import calendar
+
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    customer_id = Column(Integer, primary_keys = True, autoincrement = True)
+    first_name = Column(String(50), nullable = False)
+    last_name = Column(String(50), nullable = False)
+    phone = Column(String(12), nullable = False)
+    email = Column(String(50), nullable = False)
+    number = Column(String(16), nullable=False)
+    expire_date = Column(Date, nullable=False)
+    adresse = Column(String(200))
+
+
+
+    

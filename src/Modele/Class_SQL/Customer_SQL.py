555598389f2170ddc70cbc866e21d3f5f5ecb46f
aantoine01 <augustin.antoine8@gmail.com>
2026-01-29 20:38:03 +0100

    Introduction de la class Customer du Model

--- src/Modele/Class_SQL/Customer_SQL.py
+++ src/Modele/Class_SQL/Customer_SQL.py
@@ -2,7 +2,6 @@ from Modele.SQLManager import Base, SessionLocal
 from sqlalchemy import ForeignKey, Column, Integer, String, Date
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import date
-import calendar
 
 
 
@@ -16,7 +15,7 @@ class Customer(Base):
     email = Column(String(50), nullable = False)
     number = Column(String(16), nullable=False)
     expire_date = Column(Date, nullable=False)
-    adresse = Column(String(200))
+    adress = Column(String(200))
 
 
 
